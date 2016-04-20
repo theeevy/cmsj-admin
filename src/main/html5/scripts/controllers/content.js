e62f85acdb72acfeb9f3ae95409bebd7166bfa04
EevyTushino <eevy.mbox@gmail.com>
2016-04-20 21:52:50 +0300

    pagination

--- src/main/html5/scripts/controllers/content.js
+++ src/main/html5/scripts/controllers/content.js
@@ -1,17 +1,33 @@
 angular.module('cmsj-admin')
-    .controller('ContentController', function($scope, $http, $location, ContentService){
-        $scope.content = {};
-        $scope.content.paging = {
-            offset : 0,
-            count : 10,
-            dir : "ASC",
-            column : "id"
+    .directive('paginationFormatter', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, element, attrs, controller) {
+                controller.$formatters.push(function(value) {
+                    return value += 1;
+                });
+                controller.$parsers.push(function(value) {
+                    return value -= 1;
+                });
+            }
         };
-        $scope.content.data = ContentService.page();
+    })
+    .controller('ContentController', function($scope, $http, $location, ContentService){
+        $scope.content = [];
+        $scope.content.$paging = {}
+        $scope.content.$paging.number = 0;
+        $scope.content.$paging.size = '10'
 
-        $scope.doLog = function(){
-            ContentService.page().$promise.then(function (data) {
-                console.log(data);
-            });
-        }
+        $scope.responseHandler = function(data){
+            data.$paging.size += '';
+            $scope.content = data;
+        };
+        ($scope.reload = function(){
+            ContentService.page({
+                offset : $scope.content.$paging.number,
+                count :  $scope.content.$paging.size,
+                dir : "ASC",
+                column : "id"
+            }).$promise.then($scope.responseHandler);
+        }).apply();
     });
