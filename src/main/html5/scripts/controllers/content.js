007b1818be4c39c06a93952558651e96e92ecf7b
EevyTushino <eevy.mbox@gmail.com>
2016-05-03 21:46:24 +0300

    20160503_2147

--- src/main/html5/scripts/controllers/content.js
+++ src/main/html5/scripts/controllers/content.js
@@ -12,18 +12,45 @@ angular.module('cmsj-admin')
             }
         };
     })
-    .controller('ContentController', function($scope, $http, $location, ContentService){
+    .controller('ContentController', function($scope, $http, $location, $state, ContentService){
         $scope.content = [];
         $scope.content.$paging = {};
         $scope.content.$paging.number = 0;
         $scope.content.$paging.size = '10';
-        $scope.selectedItem = {};
-        $scope.tinymceOptions = {};
+
+        $scope.text = 'intro_text<!-- pagebreak -->full_text';
+
+        if ($state.params.contentId !== undefined){
+            ContentService.get({ id : parseInt($state.params.contentId) }).$promise.then(function(data){
+                $scope.selectedItem = data;
+                $scope.text = $scope.selectedItem.introtext + '<!-- pagebreak -->' + $scope.selectedItem.fulltext;
+            });
+        }else{
+            $scope.selectedItem = {};
+        }
+
+        $scope.tinymceOptions = {
+            plugins: 'link image code pagebreak',
+            width: 700,
+            height: 600,
+            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | pagebreak code',
+            content_style: "img.mce-pagebreak {border: 1px dashed red;}"
+        };
+
+        console.log($state);
+
+        $scope.$watch('text', function(){
+            //console.log('text was changed!');
+            var parts = $scope.text.split('<!-- pagebreak -->');
+            $scope.selectedItem.introtext = parts[0];
+            $scope.selectedItem.fulltext = parts[1];
+        });
 
         $scope.responseHandler = function(data){
             data.$paging.size += '';
             $scope.content = data;
         };
+
         ($scope.reload = function(){
             ContentService.page({
                 offset : $scope.content.$paging.number,
