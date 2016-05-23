609039aaedb0e51adeabe86d39105156612e2de9
eevy <eevy.mbox@gmail.com>
2016-05-23 03:07:01 +0300

    strange git behavior

--- src/main/html5/scripts/controllers/main.js
+++ src/main/html5/scripts/controllers/main.js
@@ -18,4 +18,37 @@ angular.module('cmsj-admin')
             //    $log.info('Modal dismissed at: ' + new Date());
             //});
         };
+
+        $(function(){
+            var dropZone = $('#dropZone'),
+                maxFileSize = 1000000; // максимальный размер файла - 1 мб.
+            console.log(dropZone);
+            window.alert("fgh");
+
+
+            if (typeof(window.FileReader) == 'undefined') {
+                dropZone.text('Не поддерживается браузером!');
+                dropZone.addClass('error');
+            }
+
+            dropZone[0].on('dragover', function() {
+                //window.alert('dragover');
+                dropZone.addClass('hover');
+                return false;
+            });
+
+            dropZone[0].on('dragleave', function() {
+                //window.alert('dragleave');
+                dropZone.removeClass('hover');
+                return false;
+            });
+
+            dropZone[0].on('drop', function(event) {
+                //window.alert('drop');
+                event.preventDefault();
+                dropZone.removeClass('hover');
+                dropZone.addClass('drop');
+            });
+        })
+
     });
